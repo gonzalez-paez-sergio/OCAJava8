e0d7746ea00b4eecec31108b9e4342593b57795a
gonzalez.paez.sergio@gmail.com <vC6v7i8$HpY>
2019-11-12 22:37:32 -0700

    ok

--- src/main/java/c1/VeriifyingPrimitiveTypesCapacity.java
+++ src/main/java/c1/VeriifyingPrimitiveTypesCapacity.java
@@ -1,80 +1,77 @@
 package c1;
 
 /**
- * 
  * <pre>
  * things to remember
- * 
+ *
  * 1. When a number is present in the code it is called a literal; by default java
  * assumes you are defining an int
- * 
- * long max = 3123456789; // DOES NOT COMPILE * 
+ *
+ * long max = 3123456789; // DOES NOT COMPILE *
  * long max = 3123456789L; // Now java  knows it is a long
- * 
+ *
  * if there is a point inside the literal, java assumes by default it is a double
- * 
- * float f = 8.79;//Type mismatch: cannot convert from double to float * 
- * double f = 8.79;//OK 
- * 
+ *
+ * float f = 8.79;//Type mismatch: cannot convert from double to float *
+ * double f = 8.79;//OK
+ *
  * 2. primitive types hold their values in the memory where the variable is allocated
  *
  *
- *
- *
- * Definition of primitive types 
- * byte = 8 bits = [-2^7,2^7) = [-128,128) = 2^8 possible values
- * int = 32 bits = [2^31,2^31) = 2^32 possible values
- * 
- * 
  * </pre>
- * 
- * @author sergiogp
  *
+ * @author sergiogp
  */
 public class VeriifyingPrimitiveTypesCapacity {
 
-	{
-		// long max = 3123456789; // The literal 3123456789 of type int is out of range
-		long max = 3123456789L; // Now java knows it is a long
+  {
+    // long max = 3123456789; // The literal 3123456789 of type int is out of range
+    long max = 3123456789L; // Now java knows it is a long
 
-		// float f = 8.79;//Type mismatch: cannot convert from double to float
-		double f = 8.79;// OK
+    // float f = 8.79;//Type mismatch: cannot convert from double to float
+    double f = 8.79;// OK
 
-		int integer_with_underscores = 1_000_000;
-		
-		//the underscores really does not represent anything, 
-		// it is only readable syntax
-		double whatIsThis = 1_00_0.0_0;
+    int integer_with_underscores = 1_000_000;
 
-		System.out.println("whatIsThis:" + whatIsThis);
-	}
+    //the underscores really does not represent anything,
+    // it is only readable syntax
+    double whatIsThis = 1_00_0.0_0;
 
-	{
-		int i = 8;
-		System.out.println("byte = " + i + " bits = [-2^" + (i - 1) + ",2^" + (i - 1) + ") = [-"
+    System.out.println("whatIsThis:" + whatIsThis);
 
-				+ Math.pow(2, (i - 1)) + "," + Math.pow(2, (i - 1)) + ") = " +
 
-				Math.pow(2, (i)) + " possible values \n");
+    /*it is possible to assign integer values to bigger variables */
+    int i1 = 10;
+    long l1 = 10;
+    float f1 = 10;
+    double d1 = 10;
+  }
 
-		i = 16;
-		System.out.println("short = " + i + " bits = [-2^" + (i - 1) + ",2^" + (i - 1) + ") = [-"
+  {
+    int i = 8;
+    System.out.println("byte:" + getValues(i));
+    i = 16;
+    System.out.println("short:" + getValues(i));
+    i = 32;
+    System.out.println("int:" + getValues(i));
+    System.out.println(Integer.MAX_VALUE);
+    System.out.println(Integer.MIN_VALUE);
+  }
 
-				+ Math.pow(2, (i - 1)) + "," + Math.pow(2, (i - 1)) + ") = " +
+  private String getValues(int i) {
+    return
 
-				Math.pow(2, (i)) + " possible values \n");
-		i = 32;
-		System.out.println("int = " + i + " bits = [-2^" + (i - 1) + ",2^" + (i - 1) + ") = [-"
+        String.format(
+            "length = %s bits = [-2^%s,2^%s - 1] = [-%s,%s]= %s possible values"
+            , i, i - 1, i - 1, Math.pow(2, i - 1), Math.pow(2, i - 1) - 1, Math.pow(2, i)
+        );
 
-				+ Math.pow(2, (i - 1)) + "," + Math.pow(2, (i - 1)) + ") = " +
 
-				Math.pow(2, (i)) + " possible values \n");
+  }
 
-	}
+  public static void main(String[] string) {
+    VeriifyingPrimitiveTypesCapacity v = new VeriifyingPrimitiveTypesCapacity();
 
-	public static void main(String[] string) {
-		VeriifyingPrimitiveTypesCapacity v = new VeriifyingPrimitiveTypesCapacity();
-
-		System.out.println();
-	}
+    System.out.println();
+  }
 }
--- src/main/java/c2/UnderstandingJavaOperators.java
+++ src/main/java/c2/UnderstandingJavaOperators.java
@@ -1,43 +1,67 @@
 package c2;
 
 /**
+ * <pre>
  * Things to remember
- * 
- * Unless overriden with parenthesis, Java operator follow order of operation by
- * decreasing order of precedence. if 2 operators have the same level of
- * precedence then java guarantee left to right evaluation
- * 
- * 
  *
+ * 1. Unless overridden with parenthesis, Java operator follow order of operation by decreasing
+ * order of precedence. if 2 operators have the same level of precedence then java guarantee left to
+ * right evaluation
+ *
+ * 2. Casting primitives is required anytime you go from a larger numerical data type into  a
+ * smaller one
+ *
+ *
+ * 3. Overflow is when a number is so large that it will no longer fit  within the data type, so the
+ * system "wraps around" to the next lowest value and counts up from there. There’s also an
+ * analogous underflow, when the number is too low to fit in  the data type.
+ * </pre>
  */
 public class UnderstandingJavaOperators {
-	private static int y;
-	{
-		y = 4;
-		/*
-		 * the last step is the upcast of 9 to double 9.0
-		 */
-		double x = 3 + 2 * y--;
 
-		System.out.println(y + "-" + x);
+  public static void main(String[] s) {
+    castingPrimitives();
+  }
 
-	}
+  private static int y;
 
-	public static int getY() {
-		return y;
-	}
+  {
+    y = 4;
+    /* all the elements are already int, so there is not promotion
+     * the last step is the promotion of 9 to double 9.0
+     */
+    double x = 3 + 2 * y--;
 
-	public static void setY(int y) {
-		UnderstandingJavaOperators.y = y;
-	}
+    System.out.println(y + "-" + x);
 
-	public static void doubleExpression() {
-		int expression = 0;
-		/**
-		 * this is the error when we try to use the two operands on the same value
-		 * src/c2/UnderstandingJavaOperators.java:20: error: unexpected type
-		 * --expression++; ^ required: variable found: value 1 error
-		 */
-		//(--expression)++;
-	}
+  }
+
+  public static void doubleExpression() {
+    int expression = 0;
+    /**
+     * this is the error when we try to use the two operands on the same value
+     * src/c2/UnderstandingJavaOperators.java:20: error: unexpected type
+     * --expression++; ^ required: variable found: value 1 error
+     */
+    //(--expression)++;
+  }
+
+  public static void castingPrimitives() {
+    int i1 = 10;//OK
+//  	int i2 = 1.0;//NOOK (incompatible types, Required: int, found: double)
+    int i2 = (int) 1.0;//OK
+
+    short s1 = 32767;//OK
+//		short s2 = 32768;//NOOK , this number is out of range for short
+    short s3 = (short) 32768;//OK , but the value assigned will be  -32768 (overflow)
+    short s4 = (short) 32769;//OK , but the value assigned will be  -32767 (overflow)
+		
+    short s5 = (short) -32768;//OK
+    short s6 = (short) -32769;//OK , but the value assigned will be  32767 (underflow )
+
+    System.out.println(s3);
+    System.out.println(s4);
+    System.out.println(s5);
+    System.out.println(s6);
+  }
 }
